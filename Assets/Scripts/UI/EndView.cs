b30c41977e154f65b3d6bfce738bcfaf1deea615
zxc <1142716989@qq.com>
2021-08-18 17:10:54 +0800

    [*] 结尾字幕动画 -by dyl

--- Assets/Scripts/UI/EndView.cs
+++ Assets/Scripts/UI/EndView.cs
@@ -2,19 +2,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndView : MonoBehaviour
 {
-    private Animation Anim { get; set; }
+    [SerializeField]
+    private float speed;
+
+    private Text text;
+    private RectTransform textTrans;
+    
 
     private void Awake()
     {
-
+        text = transform.Find("Text").GetComponent<Text>();
+        textTrans = text.transform as RectTransform;
     }
 
     private void Start()
     {
         AudioMgr.PlayMusic(Game.EndMusic);
     }
-    
+
+    private void Update()
+    {
+        var pos = textTrans.anchoredPosition;
+        pos.y += speed * Time.deltaTime;
+        textTrans.anchoredPosition = pos;
+    }
 }
