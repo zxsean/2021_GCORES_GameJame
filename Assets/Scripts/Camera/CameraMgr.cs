29afc99674f4b07dd3cf375dd9e4af2492ccf396
zxc <1142716989@qq.com>
2021-08-21 16:47:42 +0800

    [-] 删除临时资源 -by dyl

--- Assets/Scripts/Camera/CameraMgr.cs
+++ Assets/Scripts/Camera/CameraMgr.cs
@@ -6,11 +6,17 @@ public static class CameraMgr
     private static Transform CameraTrans { get; set; }
     
     private static Transform FollowTarget { get; set; }
+    
+    // private static float Radius { get; set; }
+    // private static float MoveSpeed { get; set; }
+    // private static float OffsetX { get; set; }
 
     public static void Init()
     {
         Camera = Camera.main;
         CameraTrans = Camera.transform;
+        // Radius = 5.0f;
+        // MoveSpeed = 50.0f;
     }
 
     public static void Follow(Transform trans)
@@ -36,6 +42,17 @@ public static class CameraMgr
         camPos.x = pos.x;
         camPos.y = pos.y;
         CameraTrans.localPosition = camPos;
+
+        // if (Input.GetKey(KeyCode.Keypad6))
+        // {
+        //     OffsetX += MoveSpeed * Time.deltaTime;
+        //     camPos.x = Mathf.Lerp(camPos.x, camPos.x + Radius, OffsetX / Radius);
+        //     CameraTrans.localPosition = camPos;
+        // }
+        // else
+        // {
+        //     OffsetX = 0;
+        // }
     }
 
     public static void Clear()
